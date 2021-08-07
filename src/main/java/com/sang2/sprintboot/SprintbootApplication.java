02869c1e904069462cc384ce080b3f4df91bb3c8
y2sang <37392688+y2sang@users.noreply.github.com>
2021-08-07 13:24:26 +0900

    Initial commit

---
+++ src/main/java/com/sang2/sprintboot/SprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.sang2.sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sang2/sprintboot/SprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang2.sprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
